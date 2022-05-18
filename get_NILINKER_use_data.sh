622b84b7665020efe073302ca2d4f4a0613c5ae9
Pedro Ruas <32177425+pedroruas18@users.noreply.github.com>
2022-05-18 22:31:50 +0100

    Update get_NILINKER_use_data.sh

--- get_NILINKER_use_data.sh
+++ get_NILINKER_use_data.sh
@@ -8,18 +8,18 @@ mkdir data
 cd data/
 
 # Word-concept dictionaries
-wget https://zenodo.org/record/5927300/files/word_concept.tar.gz?download=1
-tar -xvf word_concept.tar.gz?download=1
+wget https://zenodo.org/record/6561477/files/word_concept.tar.gz?download=1
+tar -xvf 'word_concept.tar.gz?download=1'
 rm 'word_concept.tar.gz?download=1'
 
 # Trained models files
-wget https://zenodo.org/record/5927300/files/nilinker_files.tar.gz?download=1
-tar -xvf nilinker_files.tar.gz?download=1
+wget https://zenodo.org/record/6561477/files/nilinker_files.tar.gz?download=1
+tar -xvf 'nilinker_files.tar.gz?download=1'
 rm 'nilinker_files.tar.gz?download=1'
 
 # Embeddings
-wget https://zenodo.org/record/5927300/files/embeddings.tar.gz?download=1
-tar -xvf embeddings.tar.gz?download=1
+wget https://zenodo.org/record/6561477/files/embeddings.tar.gz?download=1
+tar -xvf 'embeddings.tar.gz?download=1'
 rm 'embeddings.tar.gz?download=1'
 
 #wget https://zenodo.org/record/5927300/files/REEL.tar.gz?download=1
